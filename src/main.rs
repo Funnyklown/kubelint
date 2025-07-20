03222274637262298cb41de2a764f2398a950b37
funny <amorroni@protonmail.com>
2025-07-20 15:08:42 +0200

    added clap crate for nice cli stuff

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
 use std::{fs::File, io::Read};
 use anyhow::Error;
 use yaml_rust2::{YamlEmitter, YamlLoader};
+use clap::Parser;
 
+/// Small cli that lints kubernetes config files
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Path to the .yaml
+    #[arg(short, long)]
+    file: String,
+
+}
 
 fn main() -> anyhow::Result<(), Error>{  
     unsafe { std::env::set_var("RUST_BACKTRACE", "0") };
 
-    let mut test_file =  File::open("src/test.yaml")?;
+    let args = Args::parse();
+    let mut test_file =  File::open(args.file)?;
     let mut contents = String::new();
     test_file.read_to_string(&mut contents)?;
     let docs = YamlLoader::load_from_str(&contents)?;
@@ -18,7 +29,7 @@ fn main() -> anyhow::Result<(), Error>{
     let mut out_str = String::new();
     {
         let mut emitter = YamlEmitter::new(&mut out_str);
-        emitter.dump(doc)?; // dump the YAML object to a String
+        emitter.dump(doc)?;
     }
     println!("{}", out_str);
 
